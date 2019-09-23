3d352dcbf5fc58cd8e30911713c275a796fa17dc
Saurabh Takle <41586938+saurabhtakle@users.noreply.github.com>
2019-09-23 11:35:19 +0530

    Final

--- login.php
+++ login.php
@@ -1,7 +1,7 @@
 <?php
 define('DB_USER', "root"); // db user
 define('DB_PASSWORD', ""); // db password (mention your db password here)
-define('DB_DATABASE', "trydb"); // database name
+define('DB_DATABASE', "CollegePortal"); // database name
 define('DB_SERVER', "localhost"); // db server
 
 $conn = mysqli_connect(DB_SERVER,DB_USER,DB_PASSWORD,DB_DATABASE) or die("unable to connect");
