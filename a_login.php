2db38dd1e269874c3f179e85eb913810a86f61dd
Saurabh Takle <41586938+saurabhtakle@users.noreply.github.com>
2019-09-23 11:15:53 +0530

    Final

--- a_login.php
+++ a_login.php
@@ -1,10 +1,18 @@
+<?php
+
+    session_start();
+
+    if ( isset($_SESSION['id']) ) {
+        header("Location: a_home.php ");
+    }
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>College Portal</title>
     <link rel="stylesheet" href="./src/main.css">
     <link href="https://fonts.googleapis.com/css?family=Lobster&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Rajdhani:600&display=swap" rel="stylesheet">
@@ -13,17 +21,52 @@
     <div class="welcome">
         Welcome
     </div>
-    <form action="login.php" class="container" method="post">
+    <form action="login.php" class="container" method="POST">
         <p style="font-family: 'Rajdhani', sans-serif; font-size: 10mm; margin:0px">ADMIN LOGIN</p>
+
+        <?php
+            if(isset($_REQUEST['activity'])){
+                if($_REQUEST["activity"] == "error"){
+         ?>
+                <div style="display: flex; padding: 20px; flex-direction:column">
+                    <label style='color:red;'>Username and Password didn't match</label>
+                </div>
+
+         <?php           
+
+                }
+                if($_REQUEST["activity"] == "loggedout"){
+                    ?>
+                           <div style="display: flex; padding: 20px; flex-direction:column">
+                               <label style='color:green;'>You are successfully logged out.</label>
+                           </div>
+           
+                    <?php           
+           
+                    }
+
+                    if($_REQUEST["activity"] == "expired"){
+                    ?>
+                            <div style="display: flex; padding: 20px; flex-direction:column">
+                                <label style='color:green;'>You session is expired.</label>
+                            </div>
+            
+                    <?php           
+            
+                            }           
+            }
+        ?>
+        
+        
         <div style="display: flex; padding: 20px; flex-direction:column">
-            <label for="username">Username:</label>
-            <input type="text" id="a_user" class="input" required>
+            <label>Username:</label>
+            <input type="text" name="a_user" class="input" required>
         </div>
         <div style="display:flex; padding: 20px; padding-top: 0px; flex-direction:column">
-            <label for="password">Password:</label>
-            <input type="password" id="a_pwd" class="input" required>
+            <label>Password:</label>
+            <input type="password" name="a_pwd" class="input" required>
         </div>
-        <input type="button" value="Sign In" class="btn">
+        <button class="btn" type="submit">Sign In</button>
     </form>
     <!-- <div class="container">
         <div style="display:flex; width: 100%">
